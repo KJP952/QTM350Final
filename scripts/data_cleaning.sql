e297f7e70236456c8bf7b3b1e1c7a059070e2ac3
AlixMorales <amora38@emory.edu>
2024-12-03 20:34:49 -0500

    created scripts folder and sql file for cleaning

---
+++ scripts/data_cleaning.sql
@@ -0,0 +1,4 @@
+/*markdown
+# This file will contain the data cleaning
+Here, I will upload and access the data on postgre server and clean it before returning it in a cleaned version and pushing back to the repo.
+*/
